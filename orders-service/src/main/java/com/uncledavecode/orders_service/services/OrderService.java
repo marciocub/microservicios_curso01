55e4280af40cdce124845131f17d4340efbe5644
Marcio Cubilla <marciocub@gmail.com>
2024-11-16 20:04:38 -0300

    Fin del curso parte grafana prometeus linkin

--- orders-service/src/main/java/com/uncledavecode/orders_service/services/OrderService.java
+++ orders-service/src/main/java/com/uncledavecode/orders_service/services/OrderService.java
@@ -7,7 +7,10 @@ import com.uncledavecode.orders_service.model.entities.OrderItems;
 import com.uncledavecode.orders_service.model.enums.OrderStatus;
 import com.uncledavecode.orders_service.repositories.OrderRepository;
 import com.uncledavecode.orders_service.utils.JsonUtils;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,37 +20,46 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObservationRegistry observationRegistry;
 
     public OrderResponse placeOrder(OrderRequest orderRequest) {
+        Observation  inventoryObservation = Observation.createNotStarted("inventory-service", observationRegistry);
 
         //Check for inventory
-        BaseResponse result = this.webClientBuilder.build()
-                .post()
-                .uri("lb://inventory-service/api/inventory/in-stock")
-                .bodyValue(orderRequest.getOrderItems())
-                .retrieve()
-                .bodyToMono(BaseResponse.class)
-                .block();
-        if (result != null && !result.hasErrors()) {
-            Order order = new Order();
-            order.setOrderNumber(UUID.randomUUID().toString());
-            order.setOrderItems(orderRequest.getOrderItems().stream()
-                    .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
-                    .toList());
-            var savedOrder = this.orderRepository.save(order);
-            //TODO: Send message to order topic
-            this.kafkaTemplate.send("orders-topic", JsonUtils.toJson(
-                    new OrderEvent(savedOrder.getOrderNumber(), savedOrder.getOrderItems().size(), OrderStatus.PLACED)
-            ));
-            return mapToOrderResponse(savedOrder);
-        } else {
-            throw new IllegalArgumentException("Some of the products are not in stock");
-        }
+       return inventoryObservation.observe(()->{
+
+            BaseResponse result = this.webClientBuilder.build()
+                    .post()
+                    .uri("lb://inventory-service/api/inventory/in-stock")
+                    .bodyValue(orderRequest.getOrderItems())
+                    .retrieve()
+                    .bodyToMono(BaseResponse.class)
+                    .block();
+            if (result != null && !result.hasErrors()) {
+                Order order = new Order();
+                order.setOrderNumber(UUID.randomUUID().toString());
+                order.setOrderItems(orderRequest.getOrderItems().stream()
+                        .map(orderItemRequest -> mapOrderItemRequestToOrderItem(orderItemRequest, order))
+                        .toList());
+                var savedOrder = this.orderRepository.save(order);
+                //TODO: Send message to order topic
+                log.info("LLego ante de kafka");
+                this.kafkaTemplate.send("orders-topic", JsonUtils.toJson(
+                        new OrderEvent(savedOrder.getOrderNumber(), savedOrder.getOrderItems().size(), OrderStatus.PLACED)
+                ));
+                log.info("LLego despues de kafka");
+                return mapToOrderResponse(savedOrder);
+            } else {
+                throw new IllegalArgumentException("Some of the products are not in stock");
+            }
+        });
+
     }
 
     public List<OrderResponse> getAllOrders() {
